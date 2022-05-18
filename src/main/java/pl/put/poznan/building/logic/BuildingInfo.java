06a4c8e39038d6cf9cd80485a519a5f335fa616d
Daniel Jankowski <jankowskidaniel06@gmail.com>
2022-05-18 18:07:23 +0200

    changing starter project

--- src/main/java/pl/put/poznan/building/logic/BuildingInfo.java
+++ src/main/java/pl/put/poznan/building/logic/BuildingInfo.java
@@ -1,13 +1,13 @@
-package pl.put.poznan.transformer.logic;
+package pl.put.poznan.building.logic;
 
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
-public class TextTransformer {
+public class BuildingInfo {
 
     private final String[] transforms;
 
-    public TextTransformer(String[] transforms){
+    public BuildingInfo(String[] transforms){
         this.transforms = transforms;
     }
 
