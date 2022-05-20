aaa19426840cf9633eb01dbfebe2c05a3902a77a
L-asik <antoni.lasik@gmail.com>
2022-05-20 12:28:06 +0200

    Filling the Room-class

--- src/main/java/pl/put/poznan/building/model/Room.java
+++ src/main/java/pl/put/poznan/building/model/Room.java
@@ -1,28 +1,43 @@
 package pl.put.poznan.building.model;
 
 public class Room implements BuildingSpace {
+    private String name;
+    private String address; //in building???
+
     private float area;
     private float volume;
     private float lightingpower;
+
     /**
      * Calculate area of building
      */
-    public void calculateArea(){
+    public void calculateArea() {
         this.area = 0.0F;
     }
 
     /**
      * Calculating volume for building
      */
-    public void calculateVolume(){
+    public void calculateVolume() {
         this.volume = 0.0F;
     }
 
     /**
      * Calculating lighting power of building
      */
-    public void calculateLightingpower(){
+    public void calculateLightingpower() {
         this.lightingpower = 0.0F;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getName(){return this.name;}
+
+    public String getAddress(){return this.address;}
 
 }
