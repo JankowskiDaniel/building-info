d6f53b9597bc4a75213fe2cf9f922f3828228826
Daniel Jankowski <jankowskidaniel06@gmail.com>
2022-05-22 21:26:58 +0200

    edit docs

--- src/main/java/pl/put/poznan/building/model/Building.java
+++ src/main/java/pl/put/poznan/building/model/Building.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
 /**
- * Class building objects
+ * Class for buildings
  */
 public class Building extends BuildingComponent{
 
