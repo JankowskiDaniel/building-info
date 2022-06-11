3e4c55b7e71b4a3e17152b3da3f75e9c7b32f2e3
Mateusz <mateusz.miroslaw.amlecki@gmail.com>
2022-06-11 16:34:06 +0200

    Adding first mock test

--- src/test/java/pl/put/poznan/building/model/LevelTest.java
+++ src/test/java/pl/put/poznan/building/model/LevelTest.java
@@ -2,6 +2,7 @@ package pl.put.poznan.building.model;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 
@@ -14,13 +15,24 @@ class LevelTest {
     public static void setUp() {
         ArrayList<Room> rooms = new ArrayList<>();
         level = new Level(rooms);
-        level.addRoom(300,200,123);
-        level.addRoom(1000,1200,12);
-        level.addRoom(3400,100000,3000);
+//        level.addRoom(300,200,123);
+//        level.addRoom(1000,1200,12);
+//        level.addRoom(3400,100000,3000);
     }
 
     @Test
     public void testCalculateArea(){
+        Room room1 = mock(Room.class, withSettings().useConstructor(300.0, 200.0, 123.0));
+        Room room2 = mock(Room.class, withSettings().useConstructor(1000.0,1200.0,12.0));
+        Room room3 = mock(Room.class, withSettings().useConstructor(3400.0,100000.0,3000.0));
+        when(room1.getArea()).thenReturn(300.0);
+        when(room2.getArea()).thenReturn(1000.0);
+        when(room3.getArea()).thenReturn(3400.0);
+        ArrayList<Room> newRooms = new ArrayList<>();
+        newRooms.add(room1);
+        newRooms.add(room2);
+        newRooms.add(room3);
+        level.setRooms(newRooms);
         assertEquals(4700, level.calculateArea());
     }
 
