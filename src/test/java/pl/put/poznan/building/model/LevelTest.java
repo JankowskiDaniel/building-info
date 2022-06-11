d72938f3177afef78a96c9111737ab6052d3a952
Mateusz <mateusz.miroslaw.amlecki@gmail.com>
2022-06-11 22:49:51 +0200

    Adding tests with mocks objects

--- src/test/java/pl/put/poznan/building/model/LevelTest.java
+++ src/test/java/pl/put/poznan/building/model/LevelTest.java
@@ -15,16 +15,13 @@ class LevelTest {
     public static void setUp() {
         ArrayList<Room> rooms = new ArrayList<>();
         level = new Level(rooms);
-//        level.addRoom(300,200,123);
-//        level.addRoom(1000,1200,12);
-//        level.addRoom(3400,100000,3000);
     }
 
     @Test
     public void testCalculateArea(){
-        Room room1 = mock(Room.class, withSettings().useConstructor(300.0, 200.0, 123.0));
-        Room room2 = mock(Room.class, withSettings().useConstructor(1000.0,1200.0,12.0));
-        Room room3 = mock(Room.class, withSettings().useConstructor(3400.0,100000.0,3000.0));
+        Room room1 = mock(Room.class, withSettings().useConstructor(300.0, 200.0, 123.0, 1, 10.0));
+        Room room2 = mock(Room.class, withSettings().useConstructor(1000.0,1200.0,12.0, 2, 20.0));
+        Room room3 = mock(Room.class, withSettings().useConstructor(3400.0,100000.0,3000.0, 3, 30.0));
         when(room1.getArea()).thenReturn(300.0);
         when(room2.getArea()).thenReturn(1000.0);
         when(room3.getArea()).thenReturn(3400.0);
@@ -38,22 +35,72 @@ class LevelTest {
 
     @Test
     public void testCalculateVolume(){
+        Room room1 = mock(Room.class, withSettings().useConstructor(300.0, 200.0, 123.0, 1, 10.0));
+        Room room2 = mock(Room.class, withSettings().useConstructor(1000.0,1200.0,12.0, 2, 20.0));
+        Room room3 = mock(Room.class, withSettings().useConstructor(3400.0,100000.0,3000.0, 3, 30.0));
+        when(room1.getVolume()).thenReturn(200.0);
+        when(room2.getVolume()).thenReturn(1200.0);
+        when(room3.getVolume()).thenReturn(100000.0);
+        ArrayList<Room> newRooms = new ArrayList<>();
+        newRooms.add(room1);
+        newRooms.add(room2);
+        newRooms.add(room3);
+        level.setRooms(newRooms);
         assertEquals(101400, level.calculateVolume());
     }
 
     @Test
     public void testSumLightPower(){
+        Room room1 = mock(Room.class, withSettings().useConstructor(300.0, 200.0, 123.0, 1, 10.0));
+        Room room2 = mock(Room.class, withSettings().useConstructor(1000.0,1200.0,12.0, 2, 20.0));
+        Room room3 = mock(Room.class, withSettings().useConstructor(3400.0,100000.0,3000.0, 3, 30.0));
+        when(room1.getLightingpower()).thenReturn(123.0);
+        when(room2.getLightingpower()).thenReturn(12.0);
+        when(room3.getLightingpower()).thenReturn(3000.0);
+        ArrayList<Room> newRooms = new ArrayList<>();
+        newRooms.add(room1);
+        newRooms.add(room2);
+        newRooms.add(room3);
+        level.setRooms(newRooms);
         assertEquals(3135,level.sumLightPower());
     }
 
     @Test
     public void testCalculateLightPower(){
+        Room room1 = mock(Room.class, withSettings().useConstructor(300.0, 200.0, 123.0, 1, 10.0));
+        Room room2 = mock(Room.class, withSettings().useConstructor(1000.0,1200.0,12.0, 2, 20.0));
+        Room room3 = mock(Room.class, withSettings().useConstructor(3400.0,100000.0,3000.0, 3, 30.0));
+        when(room1.getLightingpower()).thenReturn(123.0);
+        when(room2.getLightingpower()).thenReturn(12.0);
+        when(room3.getLightingpower()).thenReturn(3000.0);
+        when(room1.getArea()).thenReturn(300.0);
+        when(room2.getArea()).thenReturn(1000.0);
+        when(room3.getArea()).thenReturn(3400.0);
+        ArrayList<Room> newRooms = new ArrayList<>();
+        newRooms.add(room1);
+        newRooms.add(room2);
+        newRooms.add(room3);
+        level.setRooms(newRooms);
         assertEquals(0.6670212765957447,level.calculateLightPower());
     }
-
-    //tutaj trzeba dopełnić nwm jak to zrobić z mockami pzdr antek
     @Test
-    public void testCalculateConsumption(){assertEquals(0,level.calculateConsumption());}
+    public void testCalculateConsumption(){
+        Room room1 = mock(Room.class, withSettings().useConstructor(300.0, 200.0, 123.0, 1, 1340.0));
+        Room room2 = mock(Room.class, withSettings().useConstructor(1000.0,1200.0,12.0, 2, 2000.0));
+        Room room3 = mock(Room.class, withSettings().useConstructor(3400.0,100000.0,3000.0, 3, 300000.0));
+        when(room1.getVolume()).thenReturn(200.0);
+        when(room2.getVolume()).thenReturn(1200.0);
+        when(room3.getVolume()).thenReturn(100000.0);
+        when(room1.getHeatingenergy()).thenReturn(1340.0);
+        when(room2.getHeatingenergy()).thenReturn(2000.0);
+        when(room3.getHeatingenergy()).thenReturn(300000.0);
+        ArrayList<Room> newRooms = new ArrayList<>();
+        newRooms.add(room1);
+        newRooms.add(room2);
+        newRooms.add(room3);
+        level.setRooms(newRooms);
+        assertEquals(2.9915187376725838264299802761341, level.calculateConsumption());
+    }
 
 
 
